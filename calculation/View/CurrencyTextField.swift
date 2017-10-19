1f15ef3bf6da16aa7e6721297c25b8475600af9c
Bibhuranjan Bihari <bibhu065@gmail.com>
2017-10-19 12:28:12 +0530

    Calculation Type Application

---
+++ calculation/View/CurrencyTextField.swift
@@ -0,0 +1,27 @@
+//
+//  CurrencyTextField.swift
+//  calculation
+//
+//  Created by Bibhuranjan Bihari on 19/10/17.
+//  Copyright © 2017 Bibhuranjan Bihari. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+     
+        if let p = placeholder
+        {
+        let place = NSAttributedString (string: p, attributes: [.foregroundColor:#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+         attributedPlaceholder = place
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+}
