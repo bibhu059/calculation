a96c12a110f7e5066d6f06e2ea1de950c3b28eba
Bibhuranjan Bihari <bibhu065@gmail.com>
2017-10-19 13:56:41 +0530

    Complete Calculation App Final Touch up

--- calculation/View/CurrencyTextField.swift
+++ calculation/View/CurrencyTextField.swift
@@ -12,6 +12,21 @@ import UIKit
 
 class CurrencyTextField: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.8840534687, green: 0.8784657121, blue: 0.8883432746, alpha: 0.7970675616)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView ()
     }
@@ -27,7 +42,7 @@ class CurrencyTextField: UITextField {
             backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
             layer.cornerRadius = 5.0
             textAlignment = .center
-            
+            clipsToBounds = true
             
             if let p = placeholder
             {
