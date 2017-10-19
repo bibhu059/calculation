6647083ca6143c52990953afb0f613e2538967f5
Bibhuranjan Bihari <bibhu065@gmail.com>
2017-10-19 12:58:22 +0530

    Text Field and Programmatic Calculation Button added

--- calculation/Controller/MainVC.swift
+++ calculation/Controller/MainVC.swift
@@ -10,15 +10,25 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+    @IBOutlet weak var priceTxt: CurrencyTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calBtn = UIButton(frame:CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calBtn.backgroundColor = #colorLiteral(red: 0.9393837246, green: 0.5449611443, blue: 0.01208640769, alpha: 1)
+        calBtn.setTitle("Calcualte", for: .normal)
+        calBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calBtn.addTarget(self, action: #selector(MainVC.caluclate), for: .touchUpInside)
+        wageTxt.inputAccessoryView = calBtn
+        priceTxt.inputAccessoryView = calBtn
+    }
+    @objc func caluclate ()
+    {
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
